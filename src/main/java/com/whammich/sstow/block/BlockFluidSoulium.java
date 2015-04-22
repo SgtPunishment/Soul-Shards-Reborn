7acccc8cd236c83b95cdd2010df4375c3605d215
SgtPunishment <sgtpunishment@gmail.com>
2015-04-22 21:02:37 +0100

    Updated Waila
    Updated Waila to fix a bug
    Started work on the Molten Soulium for Tinkers Compat @TehNut should
    help

---
+++ src/main/java/com/whammich/sstow/block/BlockFluidSoulium.java
@@ -0,0 +1,14 @@
+package com.whammich.sstow.block;
+
+import net.minecraft.block.material.Material;
+import net.minecraftforge.fluids.BlockFluidClassic;
+import net.minecraftforge.fluids.Fluid;
+
+public class BlockFluidSoulium extends BlockFluidClassic {
+
+	public BlockFluidSoulium(Fluid fluid, Material material) {
+		super(fluid, material);
+		// TODO Auto-generated constructor stub
+	}
+
+}
