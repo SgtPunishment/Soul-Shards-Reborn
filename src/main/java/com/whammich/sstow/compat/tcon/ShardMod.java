627ae7f84dc357f1197dc67969b398fc540e0038
SgtPunishment <sgtpunishment@gmail.com>
2015-04-27 13:07:50 +0100

    Finished Tinkers Construct Compatibility
    - Added Soul Shard modifier, shard tier determines enchantment level
    - Corrected texture naming

--- src/main/java/com/whammich/sstow/compat/tcon/ShardMod.java
+++ src/main/java/com/whammich/sstow/compat/tcon/ShardMod.java
@@ -1,21 +1,26 @@
 package com.whammich.sstow.compat.tcon;
 
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 import com.whammich.sstow.utils.Register;
+import com.whammich.sstow.utils.Utils;
 
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.*;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.*;
+import net.minecraft.util.StatCollector;
 import tconstruct.modifiers.tools.ModBoolean;
 
 public class ShardMod extends ModBoolean {
-
+	
+	 	String color;
+	    String tooltipName;
+	    
 	public ShardMod(ItemStack[] items, int effect, String tag, String c, String tip) {
 		super(items, effect, tag, c, tip);
+		color = c;
+        tooltipName = tip;
+
 	}
 
 	@Override
@@ -26,11 +31,33 @@ public class ShardMod extends ModBoolean {
 
 	@Override
 	public void modify(ItemStack[] input, ItemStack tool) {
+		if(!Utils.isShardBound(input[0])){
+			return;
+		}
 		NBTTagCompound tags = tool.getTagCompound().getCompoundTag("InfiTool");
 		tags.setBoolean(key, true);
-		addEnchantment(tool, Register.SOUL_STEALER, 1);
+		addEnchantment(tool, Register.SOUL_STEALER, Utils.getShardTier(input[0]));
+		int modifiers = tags.getInteger("Modifiers");
+        modifiers -= 1;
+        tags.setInteger("Modifiers", modifiers);
+
+		addToolTip(tool, color + String.format(StatCollector.translateToLocal("modifier.tool.shard"), enchantlevels(Utils.getShardTier(input[0]))), color + key);
 	}
 
+	public static String enchantlevels(byte input) {
+		if(input == 1){
+			return "I";
+		} else if(input == 2){
+			return "II";
+		} else if(input == 3){
+			return "III";
+		} else if(input == 4){
+			return "IV";
+		} else if(input == 5){
+			return "V";
+		}
+		return ""; 
+	}
 	
 	
 	@SuppressWarnings("rawtypes")
