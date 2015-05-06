5cb36714520e9c25ab33c0d392ed28378a90e699
SgtPunishment <sgtpunishment@gmail.com>
2015-05-06 23:03:36 +0100

    Added Tooltip to ItemLootPage
    - Changed Missing Page to Torn Page in localization
    - Added more utilities to the Utils class
    - Added Loot Page to all generated chests

--- src/main/java/com/whammich/sstow/item/ItemLootPage.java
+++ src/main/java/com/whammich/sstow/item/ItemLootPage.java
@@ -1,9 +1,14 @@
 package com.whammich.sstow.item;
 
+import java.util.List;
+
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 import com.whammich.sstow.utils.Reference;
+import com.whammich.sstow.utils.Utils;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -19,5 +24,17 @@ public class ItemLootPage extends Item {
 	@Override
 	public void registerIcons(IIconRegister iconRegister) {
 		itemIcon = iconRegister.registerIcon(Reference.MOD_ID + ":lootPage");
+	}
+
+	@SideOnly(Side.CLIENT)
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean held) {
+		if (Utils.displayShiftForDetail && !Utils.isShiftKeyDown())
+			list.add(Utils.shiftForDetails());
+
+		if (Utils.isShiftKeyDown()) {
+			list.add(stack.stackTagCompound.getString("book"));
+			list.add(Utils.localize("info.sstow.tooltip.page") + " " + stack.stackTagCompound.getInteger("page"));
+		}
 	}
 }
--- src/main/java/com/whammich/sstow/utils/Register.java
+++ src/main/java/com/whammich/sstow/utils/Register.java
@@ -247,20 +247,22 @@ public class Register {
 			stack.setTagCompound(new NBTTagCompound());
 		}
 		stack.stackTagCompound.setString("key", "drjournal:notes:note2:2");
+		stack.stackTagCompound.setString("book", Utils.localize("guide.drjournal.book.name"));
+		stack.stackTagCompound.setInteger("page", 2);
 		return stack;
 	}
 	
 	public static void registerLoot() {
 		ChestGenHooks.getInfo(ChestGenHooks.BONUS_CHEST).addItem(new WeightedRandomChestContent(addNBTToLootPage(new ItemStack(ItemLootPage)), 1, 3, 10));
-		ChestGenHooks.getInfo(ChestGenHooks.DUNGEON_CHEST).addItem(new WeightedRandomChestContent(new ItemStack(ItemLootPage), 1, 3, 10));
-		ChestGenHooks.getInfo(ChestGenHooks.MINESHAFT_CORRIDOR).addItem(new WeightedRandomChestContent(new ItemStack(ItemLootPage), 1, 3, 10));
-		ChestGenHooks.getInfo(ChestGenHooks.PYRAMID_DESERT_CHEST).addItem(new WeightedRandomChestContent(new ItemStack(ItemLootPage), 1, 3, 10));
-		ChestGenHooks.getInfo(ChestGenHooks.PYRAMID_JUNGLE_CHEST).addItem(new WeightedRandomChestContent(new ItemStack(ItemLootPage), 1, 3, 10));
-		ChestGenHooks.getInfo(ChestGenHooks.PYRAMID_JUNGLE_DISPENSER).addItem(new WeightedRandomChestContent(new ItemStack(ItemLootPage), 1, 3, 10));
-		ChestGenHooks.getInfo(ChestGenHooks.STRONGHOLD_CORRIDOR).addItem(new WeightedRandomChestContent(new ItemStack(ItemLootPage), 1, 3, 10));
-		ChestGenHooks.getInfo(ChestGenHooks.STRONGHOLD_CROSSING).addItem(new WeightedRandomChestContent(new ItemStack(ItemLootPage), 1, 3, 10));
-		ChestGenHooks.getInfo(ChestGenHooks.STRONGHOLD_LIBRARY).addItem(new WeightedRandomChestContent(new ItemStack(ItemLootPage), 1, 3, 100));
-		ChestGenHooks.getInfo(ChestGenHooks.VILLAGE_BLACKSMITH).addItem(new WeightedRandomChestContent(new ItemStack(ItemLootPage), 1, 3, 10));
+		ChestGenHooks.getInfo(ChestGenHooks.DUNGEON_CHEST).addItem(new WeightedRandomChestContent(addNBTToLootPage(new ItemStack(ItemLootPage)), 1, 3, 10));
+		ChestGenHooks.getInfo(ChestGenHooks.MINESHAFT_CORRIDOR).addItem(new WeightedRandomChestContent(addNBTToLootPage(new ItemStack(ItemLootPage)), 1, 3, 10));
+		ChestGenHooks.getInfo(ChestGenHooks.PYRAMID_DESERT_CHEST).addItem(new WeightedRandomChestContent(addNBTToLootPage(new ItemStack(ItemLootPage)), 1, 3, 10));
+		ChestGenHooks.getInfo(ChestGenHooks.PYRAMID_JUNGLE_CHEST).addItem(new WeightedRandomChestContent(addNBTToLootPage(new ItemStack(ItemLootPage)), 1, 3, 10));
+		ChestGenHooks.getInfo(ChestGenHooks.PYRAMID_JUNGLE_DISPENSER).addItem(new WeightedRandomChestContent(addNBTToLootPage(new ItemStack(ItemLootPage)), 1, 3, 10));
+		ChestGenHooks.getInfo(ChestGenHooks.STRONGHOLD_CORRIDOR).addItem(new WeightedRandomChestContent(addNBTToLootPage(new ItemStack(ItemLootPage)), 1, 3, 10));
+		ChestGenHooks.getInfo(ChestGenHooks.STRONGHOLD_CROSSING).addItem(new WeightedRandomChestContent(addNBTToLootPage(new ItemStack(ItemLootPage)), 1, 3, 10));
+		ChestGenHooks.getInfo(ChestGenHooks.STRONGHOLD_LIBRARY).addItem(new WeightedRandomChestContent(addNBTToLootPage(new ItemStack(ItemLootPage)), 1, 3, 100));
+		ChestGenHooks.getInfo(ChestGenHooks.VILLAGE_BLACKSMITH).addItem(new WeightedRandomChestContent(addNBTToLootPage(new ItemStack(ItemLootPage)), 1, 3, 10));
 	}
 	
 }
