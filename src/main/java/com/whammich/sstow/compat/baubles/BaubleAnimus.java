a40bc4bcdeee5907437dc6c1f5ff7d1713f6d72d
SgtPunishment <sgtpunishment@gmail.com>
2015-05-11 00:44:02 +0100

    Finished Amulet of Conservo mechanics
    - Added audio cues to both

--- src/main/java/com/whammich/sstow/compat/baubles/BaubleAnimus.java
+++ src/main/java/com/whammich/sstow/compat/baubles/BaubleAnimus.java
@@ -1,17 +1,23 @@
 package com.whammich.sstow.compat.baubles;
 
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.DamageSource;
+import net.minecraft.world.World;
 import baubles.api.BaubleType;
 import baubles.api.IBauble;
+import baubles.common.container.InventoryBaubles;
+import baubles.common.lib.PlayerHandler;
 
 import com.whammich.sstow.utils.Reference;
 import com.whammich.sstow.utils.Register;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
- 
+
 public class BaubleAnimus extends Item implements IBauble {
 
 	public BaubleAnimus() {
@@ -23,38 +29,66 @@ public class BaubleAnimus extends Item implements IBauble {
 	}
 
 	@Override
+	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+		if(!world.isRemote) { 
+			InventoryBaubles baubles = PlayerHandler.getPlayerBaubles(player);
+			for(int i = 0; i < baubles.getSizeInventory(); i++) {
+				if(baubles.getStackInSlot(i) == null && baubles.isItemValidForSlot(i, stack)) {
+					baubles.setInventorySlotContents(i, stack.copy());
+					if(!player.capabilities.isCreativeMode){
+						player.inventory.setInventorySlotContents(player.inventory.currentItem, null);
+					}
+					onEquipped(stack, player);
+					break;
+				}
+			}
+			
+			if(stack.stackTagCompound == null){
+				stack.setTagCompound(new NBTTagCompound());
+				stack.stackTagCompound.setString("owner", player.getDisplayName());
+				player.attackEntityFrom(DamageSource.cactus, 19F);
+			}
+		}
+
+		return stack;	
+	}
+
+	@Override
 	@SideOnly(Side.CLIENT)
 	public boolean hasEffect(ItemStack stack, int pass) {
 		return true;
 	}
-	
+
 	@Override
-	public BaubleType getBaubleType(ItemStack arg0) {
+	public BaubleType getBaubleType(ItemStack stack) {
 		return BaubleType.AMULET;
 	}
 
 	@Override
-	public void onEquipped(ItemStack arg0, EntityLivingBase arg1) {
-		
+	public void onEquipped(ItemStack stack, EntityLivingBase player) {
+
+		if (!player.worldObj.isRemote) {
+			player.worldObj.playSoundAtEntity(player, "random.orb", 0.1F, 1.3f);
+		}		
 	}
 
 	@Override
-	public void onUnequipped(ItemStack arg0, EntityLivingBase arg1) {
-		
+	public void onUnequipped(ItemStack stack, EntityLivingBase player) {
+
 	}
 
 	@Override
-	public void onWornTick(ItemStack arg0, EntityLivingBase arg1) {
-		
+	public void onWornTick(ItemStack stack, EntityLivingBase player) {
+
 	}
 
 	@Override
-	public boolean canEquip(ItemStack arg0, EntityLivingBase arg1) {
+	public boolean canEquip(ItemStack stack, EntityLivingBase player) {
 		return true;
 	}
 
 	@Override
-	public boolean canUnequip(ItemStack arg0, EntityLivingBase arg1) {
+	public boolean canUnequip(ItemStack stack, EntityLivingBase player) {
 		return true;
 	}
 }
--- src/main/java/com/whammich/sstow/compat/baubles/BaubleConservo.java
+++ src/main/java/com/whammich/sstow/compat/baubles/BaubleConservo.java
@@ -1,10 +1,14 @@
 package com.whammich.sstow.compat.baubles;
 
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import baubles.api.BaubleType;
 import baubles.api.IBauble;
+import baubles.common.container.InventoryBaubles;
+import baubles.common.lib.PlayerHandler;
 
 import com.whammich.sstow.utils.Reference;
 import com.whammich.sstow.utils.Register;
@@ -21,35 +25,57 @@ public class BaubleConservo  extends Item implements IBauble {
 		this.setUnlocalizedName("sstow.bauble.conservo");
 		this.setTextureName(Reference.MOD_ID + ":baubleConservo");
 	}
+
+	@Override
+	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+		if(!world.isRemote) { 
+			InventoryBaubles baubles = PlayerHandler.getPlayerBaubles(player);
+			for(int i = 0; i < baubles.getSizeInventory(); i++)
+				if(baubles.getStackInSlot(i) == null && baubles.isItemValidForSlot(i, stack)) {
+					baubles.setInventorySlotContents(i, stack.copy());
+					if(!player.capabilities.isCreativeMode){
+						player.inventory.setInventorySlotContents(player.inventory.currentItem, null);
+					}
+					onEquipped(stack, player);
+					break;
+				}
+		}
+
+		return stack;	
+	}
 	
 	@Override
 	@SideOnly(Side.CLIENT)
 	public boolean hasEffect(ItemStack stack, int pass) {
 		return true;
 	}
-	
+
 	@Override
-	public boolean canEquip(ItemStack arg0, EntityLivingBase entity) {
+	public boolean canEquip(ItemStack stack, EntityLivingBase player) {
 		return true;
 	}
 
 	@Override
-	public boolean canUnequip(ItemStack arg0, EntityLivingBase entity) {
+	public boolean canUnequip(ItemStack stack, EntityLivingBase player) {
 		return true;
 	}
 
 	@Override
-	public BaubleType getBaubleType(ItemStack arg0) {
+	public BaubleType getBaubleType(ItemStack stack) {
 		return BaubleType.AMULET;
 	}
 
 	@Override
-	public void onEquipped(ItemStack arg0, EntityLivingBase entity) {}
+	public void onEquipped(ItemStack stack, EntityLivingBase player) {
+		if (!player.worldObj.isRemote) {
+			player.worldObj.playSoundAtEntity(player, "random.orb", 0.1F, 1.3f);
+		}
+	}
 
 	@Override
-	public void onUnequipped(ItemStack arg0, EntityLivingBase entity) {}
+	public void onUnequipped(ItemStack stack, EntityLivingBase player) {}
 
 	@Override
-	public void onWornTick(ItemStack arg0, EntityLivingBase entity) {}
+	public void onWornTick(ItemStack stack, EntityLivingBase player) {}
 
 }
